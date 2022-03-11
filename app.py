babf213ce7f98b29e587e0a0511be9858f1756d3
Laya <spi791017@gmail.com>
2022-03-12 01:36:16 +0800

    fix - buttons

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate
+    MessageEvent, TextMessage, TextSendMessage,ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackAction,MessageAction,URIAction
 )
 
 app = Flask(__name__)
