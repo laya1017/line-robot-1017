8900a13683a8f5d0b6fb53599f1637f4f3400f47
Laya <spi791017@gmail.com>
2022-04-04 20:29:03 +0800

    fix bugs

--- search.py
+++ search.py
@@ -194,4 +194,3 @@ def NosListFiltWords(NosList,words):
         result.append(temp.index[i] + "：\n" + temp['Contents'][i] +
              "\n處罰：" + temp["Punishment"][i].strip("\n") + "\n註記：\n" + temp["Remark"][i] + "\n")
     return "".join(result).strip("\n")
-print(NosListFiltWords(["第14條"],""))
