b701e50862d1fccd86384ce108f0c4dd10505ad2
Laya <spi791017@gmail.com>
2022-04-04 22:16:58 +0800

    debugs

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ def handle_message(event):
             if "拒測" in msg :
                 msg.replace("拒測","")
                 result = search.NosFiltWords("35,4",msg) + search.NosFiltWords("35,5",msg) + search.NosFiltWords("73,3",msg)
-            elif "累犯" or "累" :
+            elif "累犯" or "累" in msg:
                 result = search.NosFiltWords("35,5",msg)
             else :
                 result = search.NosFiltWords("35",msg)
