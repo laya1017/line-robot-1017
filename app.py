46d6c04cc00bcaed9871db28376e419e3f08baeb
Laya <spi791017@gmail.com>
2022-04-12 02:34:06 +0800

    fix keywords

--- app.py
+++ app.py
@@ -372,9 +372,9 @@ def handle_message(event):
                     result = search.NosFiltWords("35,1",msg + " 藥") + "\n" + search.NosFiltWords("35,3",msg) + "\n" + search.NosFiltWords("35,7",msg)
                 reply = TextSendMessage(text=result)
             elif "酒精" in msg and "鎖" in msg:
-                msg = msg.replace("酒精"," ")
-                msg = msg.replace("鎖"," ")
-                result = search.NosFiltWords("35-1",msg)
+                msg = msg.replace("酒精","")
+                msg = msg.replace("鎖","")
+                result = search.NosFiltWords("35-1",msg+" 點火自動")
             elif "無照" in msg :
                 msg = msg.replace("無照"," 未領有駕駛執照駕")
                 if "動力" in msg :
