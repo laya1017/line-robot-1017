1e01f6ce4ac35655b6217791e70e4bd0064c1cff
Laya <spi791017@gmail.com>
2022-04-21 21:15:18 +0800

    Add OverWeightrange

--- app.py
+++ app.py
@@ -1360,6 +1360,7 @@ def handle_message(event):
                 msg = msg.replace("危駕","")
                 result = search.NosFiltWords("43",msg)+"\n"+search.NosFiltWords("73,1,4",msg)
             elif "超重" in msg:
+                msg = msg.replace("超重","")
                 result = search.NosFiltWords("29-2,1",msg)+"\n"+search.NosFiltWords("29-2,2",msg)
             elif "拒磅" in msg:
                 result = search.getByNos("29-2,4")
