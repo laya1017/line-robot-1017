633a419b273fbdbc5f39607e3d6598661477d296
Laya <spi791017@gmail.com>
2022-04-29 10:46:07 +0800

    顯示ID

--- app.py
+++ app.py
@@ -1110,8 +1110,8 @@ def handle_message(event):
     print("使用者：",user_name,"UserId：",uid)
     if user_name not in name_list :
         reply = TextSendMessage(text="抱歉，您並非認證之成員，請洽管理員登記，謝謝。")
-    elif msg == "告訴我ID":
-        reply = TextSendMessage(text=("使用者：" + user_name + "UserId：" + uid))
+        if  msg == "告訴我ID":
+            reply = TextSendMessage(text=("使用者：" + user_name + "UserId：" + uid))
     elif msg == "Machine":
         reply = TextSendMessage(text="道安規則§83-2：\n動力機械行駛於道路時，其駕駛人必須領有小型車以上之駕駛執照。但自中華民國96年1月1日起，總重量逾3.5公噸之動力機械，其駕駛人應領有大貨車以上之駕駛執照；自中華民國101年1月1日起，重型及大型重型之動力機械，其駕駛人應領有聯結車駕駛執照。")
     elif msg == "HMOT":
